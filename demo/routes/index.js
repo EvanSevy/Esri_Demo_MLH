06ee4ea8c09da689f113047b54a989b75fc06204
Fernando Blanco <FBlanco@student.neumont.edu>
2019-10-06 09:21:44 -0600

    Restructured the html, and css files.
    added routing for both the home page and the 2dmap page.

--- demo/routes/index.js
+++ demo/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+
+router.get('/2dMapPage', function(req, res, next) {
+  res.redirect('/html/2dMapPage.html');
+});
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.redirect('/html/index.html');
 });
+
 
 module.exports = router;
